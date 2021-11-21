739b311f09ed9c698ad50e3df41b8ea127be23ee
unknown <angelaescariocorpuz@gmail.com>
2021-11-21 12:49:22 +0800

    creation of file and adding the instructions

---
+++ program 2 - addition quiz.py
@@ -0,0 +1,5 @@
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
