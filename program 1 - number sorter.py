79cbd136f8509bfc1ff8fb3c522825dc3ec122ab
unknown <angelaescariocorpuz@gmail.com>
2021-11-21 12:56:46 +0800

    insertion of code where it asks the user to enter four numbers

--- program 1 - number sorter.py
+++ program 1 - number sorter.py
@@ -1,3 +1,12 @@
-#Program 1: Number Sorter
-#Create a program that ask 4 numbers. 
-#Print the 4 numbers from highest to lowest using only if-else statement
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement
+
+def getnumbers():
+    number1 = float(input("Enter first number: "))
+    number2 = float(input("Enter second number: "))
+    number3 = float(input("Enter third number: "))
+    number4 = float(input("Enter fourth number: "))
+    return number1, number2, number3, number4
+
+numbers = getnumbers()
