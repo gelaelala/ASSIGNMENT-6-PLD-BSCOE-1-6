47ffb3299d242d6ac282b9a28f7eacf9d0c06a1f
unknown <angelaescariocorpuz@gmail.com>
2021-11-22 02:27:06 +0800

    revisions of the fourth function

--- program 1 - number sorter.py
+++ program 1 - number sorter.py
@@ -20,14 +20,14 @@ def highestnumber(num1_, num2_, num3_, num4_):
         return num4_
 
 def secondhighest (num1_, num2_, num3_, num4_):
-    max = num2_
-    if num1_ > max or num3_ < max:
-        max = num1_
-    elif num3_ > max or num4_ < max:
-        max = num3_
+    if num2_ > num1_ and num2_ > num3_:
+        return num2_
+    elif num2_ < num3_:
+        return num3_
+    elif num2 > num4_:
+        return num4_
     else:
-        max = num4_
-    return max
+        return num1_
 
 def thirdhighest (num1_, num2_, num3_, num4_):
     max = num3_
@@ -40,14 +40,14 @@ def thirdhighest (num1_, num2_, num3_, num4_):
     return max
 
 def lowestnumber (num1_, num2_, num3_, num4_):
-    max = num4_
-    if num1_ > max:
-        max = num1_
-    elif num2_ > max:
-        max = num2_
+    if num1_ < num2_ and num1_ < num3_:
+        return num1_
+    elif num2_ < num3_ and num2_ < num4_:
+        return num2_
+    elif num3_ < num4_:
+        return num3_
     else:
-        max = num3_
-    return max
+        return num4_
 
 def displayorder (highestnumber_, secondhighest_, thirdhighest_, lowestnumber_):
     print (f'The order of the numbers you entered from highest to lowest is: {highestnumber_}, {secondhighest_}, {thirdhighest_}, {lowestnumber_}.')
