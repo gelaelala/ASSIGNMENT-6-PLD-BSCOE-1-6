b2306b6d52cd3721639687bc999b113a95d87287
unknown <angelaescariocorpuz@gmail.com>
2021-11-21 13:16:53 +0800

    insertion of code for the first highest number

--- program 1 - number sorter.py
+++ program 1 - number sorter.py
@@ -9,4 +9,17 @@ def getnumbers():
     number4 = float(input("Enter fourth number: "))
     return number1, number2, number3, number4
 
-numbers = getnumbers()
+def highestnumber(num1_, num2_, num3_, num4_):
+    max = num1_
+    if num2_ > max:
+        max = num2_
+    elif num3_ > max:
+        max = num3_
+    else:
+        max = num4_
+    return max
+
+
+num1, num2, num3, num4 = getnumbers()
+
+highestnumber_ = highestnumber (num1, num2, num3, num4)
