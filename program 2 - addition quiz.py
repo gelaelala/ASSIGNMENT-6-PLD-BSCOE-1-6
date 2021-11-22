ccb34f6410b9e37f92a1c00254da8205690b988c
unknown <angelaescariocorpuz@gmail.com>
2021-11-22 12:32:50 +0800

    insertion of codes for the fourth and fith question as well as minor revisions on the first three functions

--- program 2 - addition quiz.py
+++ program 2 - addition quiz.py
@@ -8,47 +8,103 @@ import random
 import time
 
 def first ():
+    correct = int(1)
+    incorrect = int(0)
     num1 = (random.randint (0, 99))
     num2 = (random.randint (0, 99))
     print (f"{num1} + {num2}")
     answer =  int(input("Enter answer: "))
     time.sleep (1)
     if answer == num1 + num2:
-        if print ("Your answer is correct."):
-            return answer
+        print ("Your answer is correct.")
+        return correct
     else:
         print ("Your answer is incorrect.")
+        return incorrect
 
 def second ():
+    correct = int(1)
+    incorrect = int(0)
     num1 = (random.randint (0, 99))
     num2 = (random.randint (0, 99))
     print (f"{num1} + {num2}")
     answer =  int(input("Enter answer: "))
     time.sleep (1)
     if answer == num1 + num2:
-        if print ("Your answer is correct."):
-            return answer
+        print ("Your answer is correct.")
+        return correct
     else:
         print ("Your answer is incorrect.")
+        return incorrect
 
 def third ():
+    correct = int(1)
+    incorrect = int(0)
     num1 = (random.randint (0, 99))
     num2 = (random.randint (0, 99))
     print (f'{num1} + {num2}')
     answer = int(input("Enter answer: "))
     time.sleep (1)
     if answer == num1 + num2:
-        if print ("Your answer is correct."):
-            return answer
+        print ("Your answer is correct.")
+        return correct
     else:
         print ("Your answer is incorrect.")
+        return incorrect
 
-first ()
+def fourth ():
+    correct = int(1)
+    incorrect = int(0)
+    num1 = (random.randint (0, 99))
+    num2 = (random.randint (0, 99))
+    print (f'{num1} + {num2}')
+    answer = int(input("Enter answer: "))
+    time.sleep (1)
+    if answer == num1 + num2:
+        print ("Your answer is correct.")
+        return correct 
+    else:
+        print ("Your answer is incorrect.")
+        return incorrect
 
-time.sleep (1)
+def fifth ():
+    correct = int(1)
+    incorrect = int(0)
+    num1 = (random.randint (0, 99))
+    num2 = (random.randint (0, 99))
+    print (f'{num1} + {num2}')
+    answer = int(input("Enter answer: "))
+    time.sleep (1)
+    if answer == num1 + num2:
+        print ("Your answer is correct.")
+        return correct
+    else:
+        print ("Your answer is incorrect.")
+        return incorrect
 
-second ()
+def displayresult (first_, second_, third_, fourth_, fifth_):
+    result = first_ + second_ + third_ + fourth_ + fifth_ 
+    time.sleep(1)
+    print (f"Based from the results, your total score is {result}/5.")
+    time.sleep (1)
+    print ("Thank you for joining the quiz!")
 
-time.sleep (1)
+number1 = first ()
 
-third ()
+time.sleep (2)
+
+number2 = second ()
+
+time.sleep (2)
+
+number3 = third ()
+
+time.sleep (2)
+
+number4 = fourth ()
+
+time.sleep (2)
+
+number5 = fifth ()
+
+results = displayresult (number1, number2, number3, number4, number5)
