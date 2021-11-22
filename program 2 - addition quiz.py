7137ce46fe59c3af938595423a8bd487444f2133
unknown <angelaescariocorpuz@gmail.com>
2021-11-22 11:59:07 +0800

    insertion of code for the first question

--- program 2 - addition quiz.py
+++ program 2 - addition quiz.py
@@ -3,3 +3,17 @@
 # Let the user answer and evaluate if the user has the correct answer
 # The program will ask the user 10 addition operation
 # Display the result summary of the 10 operations (ex 9/10)
+
+import random 
+
+def first ():
+    num1 = (random.randint (0, 99))
+    num2 = (random.randint (0, 99))
+    print (f"{num1} + {num2}")
+    answer =  int(input("Enter answer: "))
+    if answer == num1 + num2:
+        print ("Correct")
+    else:
+        print ("Incorrect")
+
+first ()
