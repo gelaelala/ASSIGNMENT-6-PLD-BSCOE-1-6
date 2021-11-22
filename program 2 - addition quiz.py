6f8d93837302dc1f9853d09074d41917d1acfc19
unknown <angelaescariocorpuz@gmail.com>
2021-11-22 12:04:16 +0800

    insetion of code for the second question

--- program 2 - addition quiz.py
+++ program 2 - addition quiz.py
@@ -5,15 +5,34 @@
 # Display the result summary of the 10 operations (ex 9/10)
 
 import random 
+import time
 
 def first ():
     num1 = (random.randint (0, 99))
     num2 = (random.randint (0, 99))
     print (f"{num1} + {num2}")
     answer =  int(input("Enter answer: "))
+    time.sleep (1)
     if answer == num1 + num2:
-        print ("Correct")
+        if print ("Correct"):
+            return answer
+    else:
+        print ("Incorrect")
+
+def second ():
+    num1 = (random.randint (0, 99))
+    num2 = (random.randint (0, 99))
+    print (f"{num1} + {num2}")
+    answer =  int(input("Enter answer: "))
+    time.sleep (1)
+    if answer == num1 + num2:
+        if print ("Correct"):
+            return answer
     else:
         print ("Incorrect")
 
 first ()
+
+time.sleep (1)
+
+second ()
