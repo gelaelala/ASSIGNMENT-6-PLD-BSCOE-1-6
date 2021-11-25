1322f12d663855fc5328f6eded1397ed9d45addc
unknown <angelaescariocorpuz@gmail.com>
2021-11-25 18:45:11 +0800

    insertion of conditions 18-24 + minor revision on condition no. 5

--- program 1 - number sorter.py
+++ program 1 - number sorter.py
@@ -31,7 +31,7 @@ def sortnumber4(num1_, num2_, num3_, num4_):
                         print (f'The order of the numbers that you entered, from highest to lowest, is {num1_}, {num4_}, {num2_}, {num3_}. ')
 
 def sortnumber5(num1_, num2_, num3_, num4_):
-            if num1_ > num2_ and num1_ > num3_:
+            if num1_ > num4_ and num1_ > num3_:
                 if num4_ > num3_ and num3_ > num2_:
                         print (f'The order of the numbers that you entered, from highest to lowest, is {num1_}, {num4_}, {num3_}, {num2_}. ')
 
@@ -95,7 +95,40 @@ def sortnumber17 (num1_, num2_, num3_, num4_):
         if num4_ > num1_ and num1_ > num2_:
             print (f'The order of the numbers that you entered, from highest to lowest, is {num3_}, {num4_}, {num1_}, {num2_}.')
 
+def sortnumber18 (num1_, num2_, num3_, num4_):
+    if num3_ > num4_ and num3_ > num2_:
+        if num4_ > num2_ and num2_ > num1_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num3_}, {num4_}, {num2_}, {num1_}.')
 
+def sortnumber19 (num1_, num2_, num3_, num4_):
+    if num4_ > num1_ and num4_ > num2_:
+        if num1_ > num2_ and num2_ > num3_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num4_}, {num1_}, {num2_}, {num3_}.')
+
+def sortnumber20 (num1_, num2_, num3_, num4_):
+    if num4_ > num2_ and num4_ > num1_:
+        if num2_ > num1_ and num1_ > num3_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num4_}, {num2_}, {num1_}, {num3_}.')
+
+def sortnumber21 (num1_, num2_, num3_, num4_):
+    if num4_ > num2_ and num4_ > num3_:
+        if num2_ > num3_ and num3_ > num1_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num4_}, {num2_}, {num3_}, {num1_}.')
+
+def sortnumber22 (num1_, num2_, num3_, num4_):
+    if num4_ > num3_ and num4_ > num1_:
+        if num3_ > num1_ and num1_ > num2_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num4_}, {num3_}, {num1_}, {num2_}.')
+
+def sortnumber23 (num1_, num2_, num3_, num4_):
+    if num4_ > num3_ and num4_ > num2_:
+        if num3_ > num2_ and num2_ > num1_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num4_}, {num3_}, {num2_}, {num1_}.')
+
+def sortnumber24 (num1_, num2_, num3_, num4_):
+    if num4_ > num1_ and num4_ > num3_:
+        if num1_ > num3_ and num3_ > num2_:
+            print (f'The order of the numbers that you entered, from highest to lowest, is {num4_}, {num1_}, {num3_}, {num2_}.')
 
 num1, num2, num3, num4 = getnumbers()
 
@@ -132,3 +165,17 @@ sortnumber15 (num1, num2, num3, num4)
 sortnumber16 (num1, num2, num3, num4)
 
 sortnumber17 (num1, num2, num3, num4)
+
+sortnumber18 (num1, num2, num3, num4)
+
+sortnumber19 (num1, num2, num3, num4)
+
+sortnumber20 (num1, num2, num3, num4)
+
+sortnumber21 (num1, num2, num3, num4)
+
+sortnumber22 (num1, num2, num3, num4)
+
+sortnumber23 (num1, num2, num3, num4)
+
+sortnumber24 (num1, num2, num3, num4)
